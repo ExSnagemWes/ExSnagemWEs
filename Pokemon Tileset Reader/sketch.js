9bdd22e6bfde9b00da8495597e24f105520a3a53
ExSnagemWes <exsnagemwes@gmail.com>
2020-10-09 10:52:32 -0600

    Changed Folder Name
    Not much else to say

--- Pokemon Tileset Reader/sketch.js
+++ Pokemon Tileset Reader/sketch.js
@@ -1,4 +1,9 @@
-
+// Pokemon Map Generator
+// Trent Hatzel
+// Friday, October 9th, 2020
+//
+// Extra for Experts:
+//I encorporated a 2nd map with a functioning switch, as well as sound effects and visual overlaps, none of which have any proper effect elsewise on the function except for sensory benefits
 
 
 let map_1, collisionMap_1, map_2, collisionMap_2, active_map, active_collision, MAPX, MAPY, tile_size, wes_front, wes_back, wes_left, wes_right, grass, tall_grass, ledge, ledge_left, ledge_right, ledge_right_corner, ledge_right_middle, ledge_right_top, big_tree_bottom_left, big_tree_bottom_right, big_tree_middle_left, big_tree_middle_right, big_tree_top_left, big_tree_top_right, water_bottom, water, water_center_left, water_center_right, water_top_left, water_top_middle, water_top_right, tree_bottom, tree_top, bump;
@@ -6,7 +11,7 @@ let map_1, collisionMap_1, map_2, collisionMap_2, active_map, active_collision, 
 let playerX = 6;
 let playerY = 5;
 let playerIsFacing = "down";
-let frame = 0;
+let spriteFrame = 0;
 let flower_frame = 0;
 let left_map = true
 let shouldMapSwitch = false;
@@ -21,12 +26,12 @@ function preload() {
   collisionMap_1 = loadStrings("tiles/map_1_collision.txt");
   collisionMap_2 = loadStrings("tiles/map_2_collision.txt");
   wes_front = [loadImage("sprites/wes_ow_front.png"), loadImage("sprites/wes_ow_front_1.png"),loadImage("sprites/wes_ow_front.png"), loadImage("sprites/wes_ow_front_2.png")];
-  wes_back = [loadImage("sprites/wes_ow_back.png"), loadImage("sprites/wes_ow_back_1.png"),loadImage("sprites/wes_ow_back.png"), loadImage("sprites/wes_ow_back_2.png")]
-  wes_left = [loadImage("sprites/wes_ow_left.png"), loadImage("sprites/wes_ow_left_1.png")]
-  wes_right = [loadImage("sprites/wes_ow_right.png"), loadImage("sprites/wes_ow_right_1.png")]
+  wes_back = [loadImage("sprites/wes_ow_back.png"), loadImage("sprites/wes_ow_back_1.png"),loadImage("sprites/wes_ow_back.png"), loadImage("sprites/wes_ow_back_2.png")];
+  wes_left = [loadImage("sprites/wes_ow_left.png"), loadImage("sprites/wes_ow_left_1.png")];
+  wes_right = [loadImage("sprites/wes_ow_right.png"), loadImage("sprites/wes_ow_right_1.png")];
   grass = loadImage("tiles/grass.png");
   tall_grass = loadImage("tiles/tall_grass.png");
-  grass_layer = loadImage("tiles/grass_layer.png")
+  grass_layer = loadImage("tiles/grass_layer.png");
   ledge = loadImage("tiles/ledge_center.png");
   ledge_left = loadImage("tiles/ledge_left.png");
   ledge_right = loadImage("tiles/ledge_right.png");
@@ -50,7 +55,7 @@ function preload() {
   tree_bottom = loadImage("tiles/tree_bottom.png");
   flowers = [loadImage("tiles/flowers.png"), loadImage("tiles/flowers_1.png"), loadImage("tiles/flowers_2.png"), loadImage("tiles/flowers_3.png")];
   flower_2 = loadImage("tiles/flower.png");
-  bump = createAudio("audio/bump.mp3")
+  bump = createAudio("audio/bump.mp3");
 }
 
 function setup() {
@@ -84,7 +89,7 @@ function setup() {
   else {
     tile_size = height / MAPX;
   }
-  frameRate(6);
+  frameRate(8);
   noSmooth();
   active_map = map_1;
   active_collision = collisionMap_1;
@@ -97,7 +102,7 @@ function draw() {
   displayGroundTiles();
   displayPlayer();
   moveTimer();
-  displayOverlapTiles();
+  displayPlayerOverlapTiles();
 }
 function displayGroundTiles(){
   for (let y=0; y<MAPY; y++) {
@@ -109,12 +114,12 @@ function displayGroundTiles(){
 
       //Displays Tall Grass at location
       if (active_map[x][y] === 1) {
-        image(tall_grass, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(tall_grass, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Ledge at location
       else if (active_map[x][y] === 2) {
-        image(ledge, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(ledge, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Tree Bottom at location
@@ -124,8 +129,8 @@ function displayGroundTiles(){
 
       //Displays a Tree Bottom with a Tree Top overlapping at location
       else if (active_map[x][y] === 5) {
-        image(tree_bottom, y*tile_size, x*tile_size, tile_size, tile_size)
-        image(tree_top, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(tree_bottom, y*tile_size, x*tile_size, tile_size, tile_size);
+        image(tree_top, y*tile_size, x*tile_size, tile_size, tile_size);
       }
 
       //Displays Flowers shifting through loops
@@ -133,7 +138,7 @@ function displayGroundTiles(){
         image(grass, y*tile_size, x*tile_size, tile_size, tile_size);
         image(flowers[Math.floor(flower_frame)], y*tile_size, x*tile_size, tile_size, tile_size);
         if (flower_frame < 3){
-          flower_frame += 0.5
+          flower_frame += 0.5;
         }
         else{
           flower_frame = 0;
@@ -142,109 +147,126 @@ function displayGroundTiles(){
 
       //Displays a solid state flower
       else if (active_map[x][y] === 9) {
-        image(grass, y*tile_size, x*tile_size, tile_size, tile_size)
-        image(flower_2, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(grass, y*tile_size, x*tile_size, tile_size, tile_size);
+        image(flower_2, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Left End Ledge at location
       else if (active_map[x][y] === 21) {
-        image(ledge_left, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(ledge_left, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Right End Ledge at location
       else if (active_map[x][y] === 22) {
-        image(ledge_right, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(ledge_right, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Right Ledge Corner
       else if (active_map[x][y] === 23) {
-        image(ledge_right_corner, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(ledge_right_corner, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Right Ledge Middle
       else if (active_map[x][y] === 24) {
-        image(ledge_right_middle, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(ledge_right_middle, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Right Ledge Top
       else if (active_map[x][y] === 25) {
-        image(ledge_right_top, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(ledge_right_top, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Water Top Left
       else if (active_map[x][y] === 41) {
-        image(water_top_left, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(water_top_left, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Water Top Middle
       else if (active_map[x][y] === 42) {
-        image(water_top_middle, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(water_top_middle, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Water Top Right
       else if (active_map[x][y] === 43) {
-        image(water_top_right, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(water_top_right, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Water Center Left
       else if (active_map[x][y] === 44) {
-        image(water_center_left, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(water_center_left, y*tile_size, x*tile_size, tile_size, tile_size);
         }
       
       //Displays a plain Water tile
       else if (active_map[x][y] === 45) {
-        image(water, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(water, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
       //Displays a Water Center Right
       else if (active_map[x][y] === 46) {
-        image(water_center_right, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(water_center_right, y*tile_size, x*tile_size, tile_size, tile_size);
         }
 
+      //Displays a Water Bottom
       else if (active_map[x][y] === 48) {
         image(water_bottom, y*tile_size, x*tile_size, tile_size, tile_size);
         image(tall_grass, y*tile_size, x*tile_size, tile_size, tile_size);
               
         }
-      else if (active_map[x][y] === 53) {
-        image(big_tree_middle_left, y*tile_size, x*tile_size, tile_size, tile_size)
-        }
-      else if (active_map[x][y] === 54) {
-        image(big_tree_middle_right, y*tile_size, x*tile_size, tile_size, tile_size)
-        }
-      else if (active_map[x][y] === 55) {
-        image(big_tree_bottom_left, y*tile_size, x*tile_size, tile_size, tile_size)
-        }
-      else if (active_map[x][y] === 56) {
-        image(big_tree_bottom_right, y*tile_size, x*tile_size, tile_size, tile_size)
-        }
-      else if (active_map[x][y] === 57) {
-        image(big_tree_bottom_right, y*tile_size, x*tile_size, tile_size, tile_size)
-        image(big_tree_top_right, y*tile_size, x*tile_size, tile_size, tile_size)
         
+      //Displays Big Tree Middle Left
+      else if (active_map[x][y] === 53) {
+        image(big_tree_middle_left, y*tile_size, x*tile_size, tile_size, tile_size);
         }
+
+      //Displays Big Tree Middle Right
+      else if (active_map[x][y] === 54) {
+        image(big_tree_middle_right, y*tile_size, x*tile_size, tile_size, tile_size);
+        }
+
+      //Displays Big Tree Bottom Left
+      else if (active_map[x][y] === 55) {
+        image(big_tree_bottom_left, y*tile_size, x*tile_size, tile_size, tile_size);
+        }
+
+      //Displays Big Tree Bottom Right
+      else if (active_map[x][y] === 56) {
+        image(big_tree_bottom_right, y*tile_size, x*tile_size, tile_size, tile_size);
+        }
+
+      //Displays Big Tree Right Overlap tile
+      else if (active_map[x][y] === 57) {
+        image(big_tree_bottom_right, y*tile_size, x*tile_size, tile_size, tile_size);
+        image(big_tree_top_right, y*tile_size, x*tile_size, tile_size, tile_size);
+        }
+
+      //Displays Big Tree Left Overlap Tile
       else if (active_map[x][y] === 58) {
-        image(big_tree_bottom_left, y*tile_size, x*tile_size, tile_size, tile_size)
-        image(big_tree_top_left, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(big_tree_bottom_left, y*tile_size, x*tile_size, tile_size, tile_size);
+        image(big_tree_top_left, y*tile_size, x*tile_size, tile_size, tile_size);
         }
     }
   }
 }
 
 
-function displayOverlapTiles() {
-  //squares and numbers
+function displayPlayerOverlapTiles() {
   for (let y=0; y<MAPY; y++) {
     for (let x=0; x<MAPX; x++) {
-      //Displays the appropriate tile for the number on the map_1.txt file
+      //Displays the appropriate tile for the number on the map_1.txt files
+
+      //Displays a player overlap Tree Top
       if (active_map[x][y] === 4) {
-        image(tree_top, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(tree_top, y*tile_size, x*tile_size, tile_size, tile_size);
         }
+      
+      //Displays a player overlap Big Tree Top Left
       else if (active_map[x][y] === 51) {
-        image(big_tree_top_left, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(big_tree_top_left, y*tile_size, x*tile_size, tile_size, tile_size);
         }
+
+      //Displays a player overlap Big Tree Top Right
       else if (active_map[x][y] === 52) {
-        image(big_tree_top_right, y*tile_size, x*tile_size, tile_size, tile_size)
+        image(big_tree_top_right, y*tile_size, x*tile_size, tile_size, tile_size);
         }
       }
   }
@@ -253,131 +275,154 @@ function displayOverlapTiles() {
 
 function keyPressed(){
   let player_faced = playerIsFacing;
-  //Pressing "r" toggles run
+  //Pressing SPACEBAR toggles run
+
+  //Checks if Space was pressed
   if (key === " "){
-    if (run === false){
-      run = true;
+    run = !run;
+    //Toggles run, and checks what it is set to to change movement speed appropriately
+    if (run === true){
       moveDelay = 80;
     }
     else{
-      run = false;
       moveDelay = 130;
     }
   }
+  //Moves Left
   if (key === "a"){
     move_left();
   }
 
+  //Moves Right
   if (key === "d"){
     move_right();
   }
 
+  //Moves Up
   if (key === "w"){
     move_up();
   }
   
+  //Moves Down
   if (key === "s"){
     move_down();
   }
+  //Checks if player is facing the same direction as when they started the move sequence. If they are not, the spriteFrame variable is reset to 0 to prevent visual errors
   if (player_faced !== playerIsFacing){
-    frame = 0;
+    spriteFrame = 0;
   }
 
 }
 
 function displayPlayer(){
-  //imageMode(CENTER)
+  //Checks which direction the player is facing, and clarifies they are not moving already
+  //A gradual movement procedure (so not visually teleporting from tile to tile) was planned but cut for time to work on Final Project as it would have little value overall
+  //Note: If a key is pressed (IE [a]) and another key is pressed (IE [d]), [d] can be released with [a] pressed and keep moving in the direction left. This is not an oversight or error in the code, as the RPG overwold system in Pokemon I am replicating follows this same mechanic
   if (playerIsFacing === "down"){
     if (!(playerIsMoving)){
+      //Checks if "s" is being pressed at the present moment
       if (keyIsPressed && key === "s"){
-        if (frame < 3){
-          if (frame ===1){
+        //Checks where in the animation the sprite is, if it is far enough, it will move again
+        if (spriteFrame < 3){
+          if (spriteFrame ===1){
             move_down();
           }
-          frame ++;}
+          spriteFrame ++;}
         else{
-          frame = 0;
+          spriteFrame = 0;
           move_down();
         }
       }
+      //Checks if the Key has been released. If so, it resets the animation back to base
       else{
-        frame = 0;
+        spriteFrame = 0;
       }
     }
-    image(wes_front[frame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
+    //Displays the current frame
+    image(wes_front[spriteFrame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
   }
+  //Mirror Code: Up
   else if (playerIsFacing === "up"){
     if (keyIsPressed && key === "w"){
-      if (frame < 3){
-        if (frame ===1){
+      if (spriteFrame < 3){
+        if (spriteFrame ===1){
           move_up();
         }
-        frame ++;}
+        spriteFrame ++;}
         else{
-          frame = 0;
+          spriteFrame = 0;
           move_up();
       }
     }
     else{
-      frame = 0;
+      spriteFrame = 0;
     }
-    image(wes_back[frame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
+    image(wes_back[spriteFrame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
    }
+  //Mirror Code: Right
   else if (playerIsFacing === "right"){
     if (keyIsPressed && key === "d"){
       
       if (active_collision[playerX][playerY+1]===5){
         shouldMapSwitch = true;
       }
-      if (frame < 1){
-        frame ++;}
+      if (spriteFrame < 1){
+        spriteFrame ++;}
       else{
-        frame = 0;
+        spriteFrame = 0;
         move_right();
       }
     }
     else{
-      frame = 0;
+      spriteFrame = 0;
     }
-    image(wes_right[frame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
+    image(wes_right[spriteFrame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
   }
+
+  //Mirror Code: Left
   else if (playerIsFacing === "left"){
     if (keyIsPressed && key === "a"){
       
       if (active_collision[playerX][playerY-1]===5){
         shouldMapSwitch = true;
       }
-      if (frame < 1){
-        frame ++;}
+      if (spriteFrame < 1){
+        spriteFrame ++;}
       else{
-        frame = 0;
+        spriteFrame = 0;
         move_left();
       }
     }
     else{
-      frame = 0;
+      spriteFrame = 0;
     }
-    image(wes_left[frame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
+    image(wes_left[spriteFrame], playerY*tile_size, playerX*tile_size-(tile_size*0.3), tile_size, tile_size*1.3)
   }
-  //Checks if Player is on a Tall Grass tile and adds the grass overlay if they are
+  //Checks if Player is on a Tall Grass tile and adds the grass overlay ontop of player sprite if they are
   if (active_map[playerX][playerY] === 1 || active_map[playerX][playerY] === 48){
     image(grass_layer, playerY*tile_size, playerX*tile_size, tile_size, tile_size)
   }
 }
 
+//Moves the player left
 function move_left(){
+  //Checks to make sure the player is not in the process of moving already
   if (!(playerIsMoving)){
+    //Clarifies player is facing the direction first to prevent multiple directions being moved at once
     if (playerIsFacing === "left"){
+      //Checks the players coordinates on the active Collision Map to see if they are able to move in the selected direction
       if (active_collision[playerX][playerY-1]===0 || active_collision[playerX][playerY-1]===4 || active_collision[playerX][playerY-1]===5){
         playerY--;
         playerIsMoving = true;
         moveCounter = millis();
       }
       else{
+      //If player wants to move to a tile they cannot move to, they will instead just play the animation and a bumping sound
         bump.play()
       }
     }
     else{
+      //If player was not already facing left, now they are
       playerIsFacing = "left";
     }
   }
@@ -386,11 +431,12 @@ function move_right(){
   if (!(playerIsMoving)){
     if (playerIsFacing === "right"){
 
-
+      //Checks the players coordinates on the active Collision Map to see if they are able to move in the selected direction
       if (active_collision[playerX][playerY+1]===0 || active_collision[playerX][playerY+1]===3 || active_collision[playerX][playerY+1]===4 || active_collision[playerX][playerY+1]===5){
         playerY++;
         playerIsMoving = true;
         moveCounter = millis();
+        //Checks if the player hopped onto another ledge tile, and keeps moving if they have
         while (active_collision[playerX][playerY]===3){
           playerY++;
           }
@@ -444,23 +490,28 @@ function move_down(){
 }
 
 function map_configure(){
+  //To prevent the entire map being re-loaded every frame, it has a switch to check if it is supposed to switch or not
   if (shouldMapSwitch){
-    if (playerIsFacing === "right"){
+    //Checks which map the player is switching to, then switches to it
+      if (playerIsFacing === "left"){
+        playerY = 15
+        active_map = map_1;
+        active_collision = collisionMap_1;
+        shouldMapSwitch = false;
+      }
+    else if (playerIsFacing === "right"){
       playerY = 0
       active_map = map_2;
       active_collision = collisionMap_2;
       shouldMapSwitch = false;
     }
-    if (playerIsFacing === "left"){
-      playerY = 15
-      active_map = map_1;
-      active_collision = collisionMap_1;
-      shouldMapSwitch = false;
-    }
   }
     
 }
+
+
 function moveTimer(){
+  //Checks the speed the player should be moving at, and checks if enough time has passed for the player to move again
   if (millis()>(moveCounter+moveDelay)){
     playerIsMoving = false;
     }
